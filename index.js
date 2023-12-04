fcca66872d5abcf9a9d87bae50797ee9470dc02b
Adrian Suarez <asuarez9382@gmail.com>
2023-12-03 22:15:35 -0800

    adds comments to the pieces of code written

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-/*const url = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data) 
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
-*/
 
+//handles the submit event by taking the user input and doing a get request to the api
 function handleSubmit(event) {
     event.preventDefault()
     const userInput = document.querySelector("#search-bar")
@@ -37,6 +23,7 @@ function handleSubmit(event) {
 document.addEventListener("DOMContentLoaded", function() {
     const searchBarForm = document.querySelector("#search-bar-form")
 
+    //Adds an event listener on the search bar button 
     searchBarForm.addEventListener("submit", handleSubmit)
 })
 
